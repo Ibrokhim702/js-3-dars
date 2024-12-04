3ec2621d25e9a43c3b4f16624405cf09c1896fc8
Ibrokhim702 <rakhimovaziz077@gmail.com>
2024-12-04 21:55:40 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const number = alert("Sizda hozir random sonlar chiqadi")
+
+// alert = Math.round(randomraqam)
+
+let ism = Math.round(Math.random()* 100)
+console.log(ism);
+alert(`Random orqali chiqqan raqam:${ism}`)
+
+const savol = confirm("Biz random sonni 2ga Ko'paytirmoqchi va bo'lmoqchimiz!")
+alert("Random orqali chiqqan son:21edi biz uni 2 ga ko'paytirib 44 sonni chiqardik,va shu sonni 2 ga bo'lib:11sonni chiqardik,va shu sonni 3 ga bo'lgandagi qoldiq: 1 sonini chiqardik")
+
+// console.log(Math.round(number));
